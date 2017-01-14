394c63ce896cfd1c01dffd39bd291dbc8333eee3
Will Storey <will@summercat.com>
2017-01-14 12:04:33 -0800

    tests: Reverse indicating wanting failure to indicating wanting success

--- irc_test.go
+++ irc_test.go
@@ -8,129 +8,129 @@ func TestParseMessage(t *testing.T) {
 		prefix  string
 		command string
 		params  []string
-		fail    bool
+		success bool
 	}{
-		{":irc PRIVMSG\r\n", "irc", "PRIVMSG", []string{}, false},
+		{":irc PRIVMSG\r\n", "irc", "PRIVMSG", []string{}, true},
 
-		{":irc PRIVMSG", "", "", []string{}, true},
+		{":irc PRIVMSG", "", "", []string{}, false},
 
-		{":irc PRIVMSG one", "", "", []string{}, true},
+		{":irc PRIVMSG one", "", "", []string{}, false},
 
-		{":irc \r\n", "", "", []string{}, true},
-		{"PRIVMSG\r\n", "", "PRIVMSG", []string{}, false},
-		{"PRIVMSG :hi there\r\n", "", "PRIVMSG", []string{"hi there"}, false},
-		{": PRIVMSG \r\n", "", "", []string{}, true},
-		{"ir\rc\r\n", "", "", []string{}, true},
+		{":irc \r\n", "", "", []string{}, false},
+		{"PRIVMSG\r\n", "", "PRIVMSG", []string{}, true},
+		{"PRIVMSG :hi there\r\n", "", "PRIVMSG", []string{"hi there"}, true},
+		{": PRIVMSG \r\n", "", "", []string{}, false},
+		{"ir\rc\r\n", "", "", []string{}, false},
 
-		{":irc PRIVMSG blah\r\n", "irc", "PRIVMSG", []string{"blah"}, false},
-		{":irc 001 :Welcome\r\n", "irc", "001", []string{"Welcome"}, false},
-		{":irc 001\r\n", "irc", "001", []string{}, false},
+		{":irc PRIVMSG blah\r\n", "irc", "PRIVMSG", []string{"blah"}, true},
+		{":irc 001 :Welcome\r\n", "irc", "001", []string{"Welcome"}, true},
+		{":irc 001\r\n", "irc", "001", []string{}, true},
 
 		// This is technically invalid per grammar as there is a trailing space.
 		// However I permit it as we see trailing space in the wild frequently.
-		{":irc PRIVMSG \r\n", "irc", "PRIVMSG", []string{}, false},
+		{":irc PRIVMSG \r\n", "irc", "PRIVMSG", []string{}, true},
 
-		{":irc @01\r\n", "", "", []string{}, true},
-		{":irc \r\n", "", "", []string{}, true},
-		{":irc  PRIVMSG\r\n", "", "", []string{}, true},
+		{":irc @01\r\n", "", "", []string{}, false},
+		{":irc \r\n", "", "", []string{}, false},
+		{":irc  PRIVMSG\r\n", "", "", []string{}, false},
 
-		{":irc 000 hi\r\n", "irc", "000", []string{"hi"}, false},
+		{":irc 000 hi\r\n", "irc", "000", []string{"hi"}, true},
 
 		// It is valid to have no parameters.
-		{":irc 000\r\n", "irc", "000", []string{}, false},
+		{":irc 000\r\n", "irc", "000", []string{}, true},
 
 		// Test last param having no :
 		{":irc 000 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5\r\n", "irc", "000", []string{"1",
 			"2", "3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", "5"},
-			false},
+			true},
 
 		// Test last param having no : nor characters
 		{":irc 000 1 2 3 4 5 6 7 8 9 0 1 2 3 4 \r\n", "irc", "000",
 			[]string{"1", "2",
-				"3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", ""}, false},
+				"3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", ""}, true},
 
 		// Test last param having : but no characters
 		{":irc 000 1 2 3 4 5 6 7 8 9 0 1 2 3 4 :\r\n", "irc", "000",
 			[]string{"1", "2",
-				"3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", ""}, false},
+				"3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", ""}, true},
 
 		{":irc 000 1 2 3 4 5 6 7 8 9 0 1 2 3 4 :hi there\r\n", "irc", "000",
 			[]string{"1", "2",
 				"3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", "hi there"},
-			false},
+			true},
 
 		{":irc 000 1 2 3 4 5 6 7 8 9 0 1 2 3 4 hi there\r\n", "irc", "000",
 			[]string{"1", "2",
 				"3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", "hi there"},
-			false},
+			true},
 
 		// Malformed because \r should not appear there.
-		{":irc 000 \r\r\n", "", "", []string{}, true},
+		{":irc 000 \r\r\n", "", "", []string{}, false},
 
 		// Param must not be blank unless last param.
 		// While this violates the grammar, I permit it now anyway.
-		{":irc 000 \r\n", "irc", "000", []string{}, false},
+		{":irc 000 \r\n", "irc", "000", []string{}, true},
 
-		{":irc 000 0a 1b\r\n", "irc", "000", []string{"0a", "1b"}, false},
+		{":irc 000 0a 1b\r\n", "irc", "000", []string{"0a", "1b"}, true},
 
 		// If we have a space then there must be a parameter (unless it's the
 		// 15th).
 		// While this violates the grammar, I permit it now anyway.
-		{":irc 000 0 1 \r\n", "irc", "000", []string{"0", "1"}, false},
+		{":irc 000 0 1 \r\n", "irc", "000", []string{"0", "1"}, true},
 
-		{":irc 000 a\x00 1 \r\n", "", "", []string{}, true},
+		{":irc 000 a\x00 1 \r\n", "", "", []string{}, false},
 
 		// : inside a middle. Valid.
-		{":irc 000 a:bc\r\n", "irc", "000", []string{"a:bc"}, false},
+		{":irc 000 a:bc\r\n", "irc", "000", []string{"a:bc"}, true},
 
 		{":irc 000 hi :there yes\r\n", "irc", "000", []string{"hi", "there yes"},
-			false},
+			true},
 
 		// : inside a middle parameter. This is valid.
 		{":irc 000 hi:hi :no no\r\n", "irc", "000", []string{"hi:hi", "no no"},
-			false},
+			true},
 
 		{":irc 000 hi:hi :no no :yes yes\r\n", "irc", "000", []string{"hi:hi", "no no :yes yes"},
-			false},
+			true},
 
 		{":irc 000 hi:hi :no no :yes yes\n", "irc", "000", []string{"hi:hi", "no no :yes yes"},
-			false},
+			true},
 
 		// Fails and SHOULD actually. Trailing whitespace is not valid here.
 		// Ratbox currently does send messages like this however.
 		{":irc MODE #test +o user  \r\n", "irc", "MODE", []string{"+o", "user"},
-			true},
+			false},
 
 		// Blank topic parameter is used to unset the topic.
 		{":nick!user@host TOPIC #test :\r\n", "nick!user@host", "TOPIC", []string{"#test", ""},
-			false},
+			true},
 
 		{":nick!user@host MODE #test +o :blah\r\n", "nick!user@host", "MODE",
-			[]string{"#test", "+o", "blah"}, false},
+			[]string{"#test", "+o", "blah"}, true},
 
 		{":nick!user@host MODE #test +o blah1 :blah\r\n", "nick!user@host", "MODE",
-			[]string{"#test", "+o", "blah1", "blah"}, false},
+			[]string{"#test", "+o", "blah1", "blah"}, true},
 
 		{":nick!user@host MODE #test +o :blah1 blah\r\n", "nick!user@host", "MODE",
-			[]string{"#test", "+o", "blah1 blah"}, false},
+			[]string{"#test", "+o", "blah1 blah"}, true},
 
 		{":nick!user@host PRIVMSG #test \r\n", "nick!user@host", "PRIVMSG",
-			[]string{"#test"}, false},
+			[]string{"#test"}, true},
 
 		{":nick!user@host PRIVMSG #test :\r\n", "nick!user@host", "PRIVMSG",
-			[]string{"#test", ""}, false},
+			[]string{"#test", ""}, true},
 	}
 
 	for _, test := range tests {
 		msg, err := ParseMessage(test.input)
 		if err != nil {
-			if test.fail != true {
+			if test.success {
 				t.Errorf("ParseMessage(%q) = %s", test.input, err)
 			}
 			continue
 		}
 
-		if test.fail {
+		if !test.success {
 			t.Errorf("ParseMessage(%q) should have failed, but did not.", test.input)
 			continue
 		}
